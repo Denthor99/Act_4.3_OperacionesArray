ca7d9342aee3b6dc52b8befcf500dffbec738d65
Den <drodsan2708@g.educaand.es>
2023-02-21 20:28:18 +0100

    drodsan2708 - Subida de la activida a GitHub - 21/02/2023

--- src/Actividad43Main.java
+++ src/Actividad43Main.java
@@ -3,30 +3,69 @@ import java.util.Arrays;
 public class Actividad43Main {
     public static void main(String[] args) {
         // Crea un array con 10 números aleatorios
+        System.out.println("1. Creamos un array con 10 numeros aleatorios");
         int[]a=new int[10];
         arrayRandom(a);
         System.out.println(Arrays.toString(a));
+        System.out.println();
 
         // Imprime el array anterior empezando por la última posición
-       imprimeLast(a);
+        System.out.println("2. Imprime el array anterior empezando por la última posición");
+        imprimeLast(a);
+        System.out.println();
 
         // Ordena el array utilizando la clase de utilidades Arrays
-        Arrays.sort(a);
+        System.out.println("3. Ordena el array utilizando Array.sort");
+        int []arrayOrdenado=ordenArray(a);
+        System.out.println(Arrays.toString(arrayOrdenado));
+        System.out.println();
 
         // Busca un número recibido como parámetro dentro del array. Por dos metodos diferentes:
         // Manual
+        System.out.println("4.1. Busqueda de un numero de forma manual");
         System.out.println(manualSearch(a,25));
+        System.out.println();
         // Clase Arrays de Java
+        System.out.println("4.2. Busqueda de un numero usando el metodo binarySearch");
         System.out.println(Arrays.binarySearch(a,25));
+        System.out.println();
 
         // Compara dos arrays recibidos por parametros, devolviendo si son iguales o no
+        System.out.println("5. Comparación entre dos arrays");
         int[]b=a;
-        //arrayRandom(b);
-        System.out.println("Los arrays son iguales?= "+compararArrays(a,b));
+        System.out.println("Los arrays son iguales? "+compararArrays(a,b));
+        System.out.println();
 
         // Realiza 4 copias del array en nuevo array de cuatro posiciones
-        //int[][]copy=new int[10][3];
-        //copyArrays(a,copy);
+        // Crearemos un metodo por cada tipo de copia
+        System.out.println("6. Realizamos 4 copias del array en un nuevo array");
+        int [][] clonArray=new  int[4][0];
+        System.out.println();
+
+        //Usamos primero el metodo clone
+        System.out.println("6.1. Copia de array usando el metodo clone");
+        clonacionArrays(a,clonArray);
+        System.out.println();
+
+        //Usamoos el metodo copyOf
+        System.out.println("6.2. Copia de array usando el metodo copyOf");
+        copyOfArrays(a, clonArray);
+        System.out.println();
+
+        //Usamos el metoodo copyOfRange
+        System.out.println("6.3. Copia de un array usandoo el metodo copyOfRange");
+        copyOfRangeArrays(a,clonArray);
+        System.out.println();
+
+        //Usamos el metodo Arraycopy
+        System.out.println("6.4. Copia del array usando el metodo Arraycopy de la clase System");
+        systemArrayCopy(a,clonArray);
+        System.out.println();
+
+        //Ahora imprimimos el contenido del array
+        System.out.println("Resultadoo del ejercicio 6: ");
+        imprimirArrayClonado(clonArray);
+
     }
     // Metodo numero aleatorio
     public static int [] arrayRandom(int[]a){
@@ -57,8 +96,9 @@ public class Actividad43Main {
 
     // Metodo ordenar array
     static int[] ordenArray (int[]a){
-        Arrays.sort(a);
-        return a;
+        int[] b=a.clone();
+        Arrays.sort(b);
+        return b;
     }
 
 
@@ -79,13 +119,25 @@ public class Actividad43Main {
     }
 
     // Metodo copia de arrays
-    static int copyArrays(int []a,int[][]s){
-        for (int i = 0; i < s.length; i++) {
-            s[i][0]=a.clone();
-            s[i][1]=Arrays.copyOf(a,10);
-            s[i][2]=Arrays.copyOfRange(a,0,9);
-            s[i][3]=System.arraycopy(a,0,s,0,a.length);
-        }
-        return s;
+    static void clonacionArrays(int []original,int[][]copia){
+        copia[0]=original.clone();
+        System.out.println(Arrays.toString(copia[0]));
+    }
+    static void copyOfArrays(int []original,int[][]copia){
+        copia[1]=Arrays.copyOf(original,original.length);
+        System.out.println(Arrays.toString(copia[1]));
+    }
+    static void copyOfRangeArrays(int []original,int[][]copia){
+        copia[2]=Arrays.copyOfRange(original,0,original.length);
+        System.out.println(Arrays.toString(copia[2]));
+    }
+    static void systemArrayCopy(int []original,int[][]copia){
+        int[] systemCopy=new int[original.length];
+        System.arraycopy(original,0,systemCopy,0,original.length);
+        copia[3]=systemCopy;
+        System.out.println(Arrays.toString(copia[3]));
+    }
+    static void imprimirArrayClonado(int[][]clon){
+        System.out.println(Arrays.toString(clon[0])+", "+Arrays.toString(clon[1])+", "+Arrays.toString(clon[2])+", "+Arrays.toString(clon[3]));
     }
 }
